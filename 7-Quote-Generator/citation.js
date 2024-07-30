f5925645385d84c6393ac0ed764de6e69881982f
Seyy <147831465+SeyZiiZ@users.noreply.github.com>
2024-07-30 03:41:16 +0200

    Update citation.js
    Changed the logic of colors

--- 7-Quote-Generator/citation.js
+++ 7-Quote-Generator/citation.js
@@ -1,6 +1,4 @@
 import { citations } from "./citation2.js";
-import { background } from "./citation2.js";
-
 const container = document.querySelector("#citation-container");
 const quoteBtn = document.querySelector("#button");
 
@@ -12,13 +10,14 @@ const getCitation = () => {
 
     container.innerHTML = 
     `
-    <h2>${citations[i].text}</h2>
+    <h2>" ${citations[i].text} "</h2>
     <p>- ${citations[i].author}</p>
     `;
 
     console.log(i);
 
-    const color = background[i - 1].backgroundColor;
+    let j = i === 0 ? 0 : i;
+    const color = citations[j].backgroundColor;
     document.body.style.backgroundColor = color;
 }
 
@@ -26,7 +25,7 @@ quoteBtn.addEventListener("click", getCitation);
 
 container.innerHTML = 
     `
-        <h2>${citations[0].text}</h2>
+        <h2>" ${citations[0].text} "</h2>
         <p>- ${citations[0].author}</p>
     `;
-
+document.body.style.backgroundColor = '#77B1A9';
