e48a5514260ee2ea440617cd4b79fada0663f7c0
Seyy <147831465+SeyZiiZ@users.noreply.github.com>
2024-08-07 14:14:21 +0200

    Add JS & CSS
    JS is using fetch API calls to display your researches

---
+++ 9-Pokemon-Search-App/index.js
@@ -0,0 +1,73 @@
+const fetchDiv = document.getElementById("fetch-div");
+
+const searchInput = document.getElementById("search-input");
+const searchButton = document.getElementById("search-button");
+const pokemonInfoDiv = document.querySelector(".pokemon-info");
+const pokemonName = document.getElementById("pokemon-name");
+const pokemonId = document.getElementById("pokemon-id");
+const pokemonWeight = document.getElementById("weight");
+const pokemonHeight = document.getElementById("height");
+const pokemonType = document.getElementById("types");
+const pokemonHP = document.getElementById("hp");
+const pokemonAttack = document.getElementById("attack");
+const pokemonDefense = document.getElementById("defense");
+const pokemonSpecialAttack = document.getElementById("special-attack");
+const pokemonSpecialDefense = document.getElementById("special-defense");
+const pokemonSpeed = document.getElementById("speed");
+
+const getPokemonUser = (searchInputValue) => {
+    let searchInputCleaned = searchInputValue.toLowerCase().replace(/[^a-z]/g, '');
+    console.log(searchInputCleaned);
+    return searchInputCleaned;
+};
+
+async function fetchData(userPokemon) {
+    try {
+        const res = await fetch(`https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${userPokemon}`);
+        const data = await res.json();
+        return data;
+    } catch (err) {
+        console.log(`Error : ${err}`);
+        return null;
+    }
+}
+
+const getData = (data) => {
+    if (data) {
+        const { name, id, height, weight, types, stats, sprites } = data;
+        const typesNames = types.map(typeInfo => typeInfo.type.name);
+
+        pokemonName.innerText = `Nom : ${name}`;
+        pokemonId.innerText = `Id : ${id}`;
+        pokemonWeight.innerText = `Poids : ${weight}`;
+        pokemonHeight.innerText = `Taille : ${height}`;
+        pokemonType.innerText = `Type : ${typesNames}`;
+        pokemonHP.innerText = `HP : ${stats[0].base_stat}`;
+        pokemonAttack.innerText = `Attack : ${stats[1].base_stat}`;
+        pokemonDefense.innerText = `Defense : ${stats[2].base_stat}`;
+        pokemonSpecialAttack.innerText = `Special Attack : ${stats[3].base_stat}`;
+        pokemonSpecialDefense.innerText = `Special Defense : ${stats[4].base_stat}`;
+        pokemonSpeed.innerText = `Speed : ${stats[5].base_stat}`;
+
+        fetchDiv.innerHTML = 
+        `
+            <img src="${sprites.front_default}" alt="${name} picture" width="250px" >
+        `;
+    } else {
+        pokemonName.innerText = 'Error fetching data';
+    }
+}
+
+searchButton.addEventListener('click', async () => {
+    const userInput = searchInput.value;
+
+    const data = await fetchData(getPokemonUser(userInput));
+    console.log(data);
+    getData(data);
+});
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === 'Enter') {
+        searchButton.click();
+    }
+});
